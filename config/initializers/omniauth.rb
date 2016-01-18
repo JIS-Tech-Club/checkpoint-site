bcc171fb1d2668ba1b5b8b4db60cb6272da77f74
not-my-fault <christophertakagi@yahoo.com>
2016-01-18 15:26:46 +0700

    LOGIN WORKS BITCHES

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '217946410175-s12edjfn5pgorv1trfl45oc058eebs5l.apps.googleusercontent.com
-', 'whhhpG4yGaPWzwDPqV-Mh6lq
-', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-end
+  provider :google_oauth2, '563405389028-4g1o13so9rhclpc1c7h1cclurk0bdj5m.apps.googleusercontent.com',
+   '87nTg1uZSpoXQYwJ2Sdn0b_y',
+   {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
-Rails.application.routes.draw do
-
-
-  get 'home/show'
-
-  get 'auth/:provider/callback', to: 'sessions#create'
+CheckpointSite::Application.routes.draw do
+  get   'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
