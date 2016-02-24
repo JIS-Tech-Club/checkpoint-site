82a7fc1c2f32544eb1c0e004fa5f6e04da0f65fc
Kanha Prasad <41562@jisedu.or.id>
2016-02-24 08:39:42 +0700

    Items sorted by most recent

--- app/helpers/parents_portal_helper.rb
+++ app/helpers/parents_portal_helper.rb
@@ -2,4 +2,7 @@ module ParentsPortalHelper
   def formattedDate(time)
     time.strftime('%D')
   end
+  def sortByDate(items)
+    items.sort_by &:created_at
+  end
 end
--- app/helpers/student_portal_helper.rb
+++ app/helpers/student_portal_helper.rb
@@ -2,4 +2,7 @@ module StudentPortalHelper
   def formattedDate(time)
     time.strftime('%D')
   end
+  def sortByDate(items)
+    items.sort_by &:created_at
+  end
 end
